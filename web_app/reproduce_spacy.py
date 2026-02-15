6598faf9b84d8d39b5fd39d2e05048c84f909be4
Georgi Bozhkov <gogobozhkov@gmail.com>
2026-02-15 22:41:21 +0200

    deletion of unneeded file

--- web_app/reproduce_spacy.py
+++
@@ -1,9 +0,0 @@
-import sys
-print("Importing spacy...")
-try:
-    import spacy
-    print("SUCCESS: spacy")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    print(f"FAILED: spacy - {e}")
