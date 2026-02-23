1775e8306e4def3f11ba52b181427901805181f6
Georgi Bozhkov <gogobozhkov@gmail.com>
2026-02-24 00:50:13 +0200

    switch to the llama 3.3 model

--- text-analysis/llm_info.py
+++ text-analysis/llm_info.py
@@ -17,7 +17,7 @@ def get_llm():
         _llm = ChatCloudflareWorkersAI(
             account_id=os.getenv("CF_ACCOUNT_ID"),
             api_token=os.getenv("CF_AI_API_KEY"),
-            model="@cf/meta/llama-3.1-8b-instruct",
+            model="@cf/meta/llama-3.3-70b-instruct-fp8-fast",
             temperature=0.8,
             top_p=0.95,
             max_tokens=4096
